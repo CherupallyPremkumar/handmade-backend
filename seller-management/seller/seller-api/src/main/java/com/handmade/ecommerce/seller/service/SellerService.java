4f7ea2bdd8522ccdec2d35bdf4cd08ba61d34090
Prem Kumar Cherupally <premkumar.cherupally@gmail.com>
2025-12-25 06:03:50 -0600

    feat: Add platform state machine configuration
    - Created platform-states.xml following chenile process-states.xml pattern
    - Uses <states> and <flow> structure with <manual-state> elements
    - States: BOOTSTRAPPING, ACTIVE, SUSPENDED, MAINTENANCE, DELETED
    - Transitions: activate, suspend, reactivate, enterMaintenance, exitMaintenance, delete
    - Self-transitions for updates: updateBrandIdentity, reviseCommission, publishLegalTerms, updateFeatureConfig

--- seller-management/seller/seller-api/src/main/java/com/handmade/ecommerce/seller/service/SellerService.java
+++ seller-management/seller/seller-api/src/main/java/com/handmade/ecommerce/seller/service/SellerService.java
@@ -2,6 +2,5 @@ import com.handmade.ecommerce.seller.model.Seller;
 import org.chenile.workflow.service.StateEntityService;
 
 public interface SellerService extends StateEntityService<Seller> {
-
     void validate(CreateSellerRequest request);
 }
